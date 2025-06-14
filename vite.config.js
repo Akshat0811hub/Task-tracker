70ddd9b9822057a94809ae50c5c3ed966ed9ce56
Akshat0811 <singhakshat875@gmail.com>
2025-06-14 09:27:13 +0530

    Fix line endings

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Task-tracker/', // 👈 Yeh line add karni hai
+  base: '/Task-tracker/', // 👈 Ye repo ka naam hai, yahi hona chahiye
 });
